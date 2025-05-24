34d1a72f526f6b1c9710ed84edc80b59880f99c3
vishal yadav <manasvistaff.vishalyadav@gmail.com>
2025-05-24 11:39:59 +0530

    first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/login", { email, password });
+      const res = await axios.post("https://vyanweb-blog-backend.onrender.com/api/login", { email, password });
       login(res.data.user, res.data.token);
       navigate("/");
     } catch (err) {
